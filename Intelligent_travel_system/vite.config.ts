d9c289cb7e96f0deebaffc24bbd69de16326f3d0
PZT <15555605+pzt_zitao@user.noreply.gitee.com>
2026-01-03 13:25:39 +0800

    a little

--- Intelligent_travel_system/vite.config.ts
+++ Intelligent_travel_system/vite.config.ts
@@ -12,9 +12,7 @@ export default defineConfig({
         target: 'http://14.103.124.109:8080', 
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''), // 如果后端接口不需要 /api 前缀，请取消注释这行
-        // 注意：根据你的 request.ts 和 chatStore，前端发出的请求带 /api
-        // 如果后端路由是 /user/login 而不是 /api/user/login，则必须开启 rewrite
-        rewrite: (path) => path.replace(/^\/api/, '')
+        // rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
