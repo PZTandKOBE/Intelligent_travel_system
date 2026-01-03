d247d3b3a447d739b090c6478255e63844e29f20
PZT <15555605+pzt_zitao@user.noreply.gitee.com>
2026-01-03 16:27:53 +0800

    alittle

--- Intelligent_travel_system/vite.config.ts
+++ Intelligent_travel_system/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        // ✅ 更新为新的后端地址 (注意：如果后端是 http://123.57.85.75/api，这里通常填 IP 即可，Vite 会自动拼接 /api)
+        // ✅ 更新为新的后端地址 (注意：如果后端是 http://14.103.124.109:8080/api，这里通常填 IP 即可，Vite 会自动拼接 /api)
         target: 'http://14.103.124.109:8080', 
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '') // 保持注释，保留 /api 前缀
